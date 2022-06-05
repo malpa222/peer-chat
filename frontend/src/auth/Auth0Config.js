fd647cf563aa2919f2614bcc7b6cf7cbb4c7e6a2
Daniel Lewandowski <d.lewandowski@student.fontys.nl>
2022-06-05 19:07:28 +0200

    Build services individually

--- frontend/src/auth/Auth0Config.js
+++ frontend/src/auth/Auth0Config.js
@@ -3,7 +3,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const Auth0Config = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENTID;
 
   return (
     <Auth0Provider
